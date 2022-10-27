6ddb1f82c5b08081dc4a58389a5fa1b49043e5c6
Srishti Kamra <srishtikamra07@gmail.com>
2022-10-27 09:37:26 -0500

    Created a text field with the title and then made the background dark

--- Tic Tac Toe/ContentView.swift
+++ Tic Tac Toe/ContentView.swift
@@ -10,11 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    Text("Tic Tac Toe")
+                .font(.title)
+                .fontWeight(.bold)
+            
         }
+        .preferredColorScheme(.dark)
         .padding()
     }
 }
